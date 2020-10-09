529a2960e2227daac33fb80ee2100c767acbc460
Abimbola Oladele <abimbola.oladele@thebulb.africa>
2020-10-09 12:01:00 +0100

    Started my stop wtach project

---
+++ app.js
@@ -0,0 +1,7 @@
+const timer = 60;
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById("seconds");
+const hours = document.getElementById("hours");
+const milliSeconds = document.getElementById("milli-seconds")
+
+console.log(hours.textContent)
